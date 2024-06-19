f160953eb5cae57b803ef49e8d38f9c04cd5c5f0
Zandae0 <169157233+Zandae0@users.noreply.github.com>
2024-06-19 23:29:23 +0700

    update

--- Cloud Computing/server-side/src/controllers/translateController.js
+++ Cloud Computing/server-side/src/controllers/translateController.js
@@ -9,10 +9,8 @@ router.post('/', upload.single('video'), async (req, res, next) => {
   try {
     const videoBuffer = req.file.buffer;
 
-    // Proses video dan dapatkan hasil transkripsi
     const transcript = await videoProcessing.processVideo(videoBuffer);
 
-    // Kirim respons JSON dengan hasil transkripsi
     res.status(200).json({ success: transcript });
   } catch (error) {
     next(error);
